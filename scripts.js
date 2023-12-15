592232dae2bf3f44dde289fe5ce98de213ffe62f
Austin Welsh-Graham <austinwelshgraham.atts@gmail.com>
2023-12-15 20:24:57 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,3 +7,31 @@ function showMenu() {
 function hideMenu() {
     navigationLinks.style.right = "-200px";
 }
+
+function contactFunction() {
+    $.ajax({
+        url:"https://api.apispreadsheets.com/data/ZdB3MVK6BzDxqJiB/",
+        type:"post",
+        data:$("#contactForm").serializeArray(),
+        success: function(){
+          alert("Form Data Submitted :)")
+        },
+        error: function(){
+          alert("There was an error :(")
+        }
+    });
+}
+
+function applyFunction() {
+    $.ajax({
+        url:"https://api.apispreadsheets.com/data/gk2q49jUB7NtLWsm/",
+        type:"post",
+        data:$("#applyForm").serializeArray(),
+        success: function(){
+          alert("Form Data Submitted :)")
+        },
+        error: function(){
+          alert("There was an error :(")
+        }
+    });
+}
