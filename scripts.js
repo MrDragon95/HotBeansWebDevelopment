fdbf2af5e6578cabade1ca8687c0f2aef3b3af03
Austin Welsh-Graham <austinwelshgraham.atts@gmail.com>
2024-01-16 00:04:33 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ function hideMenu() {
 function contactFunction() {
     "use strict";
     $.ajax({
-        url: "https://api.apispreadsheets.com/data/ZdB3MVK6BzDxqJiB/",
+        url: "https://api.apispreadsheets.com/data/ZdB3MVK6BzDxqJib/",
         type: "post",
         data: $("#contactForm").serializeArray(),
         success: function () {
@@ -43,4 +43,39 @@ function applyFunction() {
             alert("An Error Has Occured. Please Try Again Later");
         }
     });
+}
+
+function ValidateForm(frm) {
+    "use strict";
+    if (frm.First_Name.value === "") {
+        alert('First name is required.');
+        frm.First_Name.focus();
+        return false;
+    }
+    if (frm.Last_Name.value === "") {
+        alert('Last name is required.');
+        frm.Last_Name.focus();
+        return false;
+    }
+    if (frm.Email_Address.value === "") {
+        alert('Email address is required.');
+        frm.Email_Address.focus();
+        return false;
+    }
+    if (frm.Email_Address.value.indexOf("@") < 1 || frm.Email_Address.value.indexOf(".") < 1) {
+        alert('Please enter a valid email address.');
+        frm.Email_Address.focus();
+        return false;
+    }
+    if (frm.Position.value === "") {
+        alert('Position is required.');
+        frm.Position.focus();
+        return false;
+    }
+    if (frm.Phone.value === "") {
+        alert('Phone is required.');
+        frm.Phone.focus();
+        return false;
+    }
+    return true;
 }
